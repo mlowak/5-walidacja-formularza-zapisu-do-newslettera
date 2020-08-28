1b532cebe3e67baf2cef716005ae05458c1d875a
mlowak <67906604+mlowak@users.noreply.github.com>
2020-08-28 13:32:24 +0200

    errors

--- js/script.js
+++ js/script.js
@@ -4,6 +4,23 @@ let allAgreeCheck = document.getElementById('all-agree');
 
 const validate = (event) => {
 
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    // trim ucina białe znaki z lewej i prawej strony stringa
+    if (txtName.value.trim() === ''){
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz imię i nazwisko!';
+
+        errors.appendChild(liError)
+        console.log(liError);
+    }
+
+
     event.preventDefault();
 
     console.log('validate()');
@@ -15,11 +32,12 @@ const allAgree = (event) => {
 
     agree1.checked = event.target.checked;
     agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
     // console.log(event.target.checked);
 }
 
-
 // nazwa funkcji, a nie wywołanie (nawiasy), żeby nie wywołała się podczas ładowania skryptu a kiedy naciśnie się na submit
 newsletterForm.addEventListener('submit', validate);
-
 allAgreeCheck.addEventListener('change', allAgree);
